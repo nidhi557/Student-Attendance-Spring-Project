085dd2acaa160070b678d70f826143fac6f9360e
Nidhi <nidhi.kedia557@gmail.com>
2019-11-12 18:07:47 +0530

    Add Project

---
+++ pro2/src/com/training/entity/TourList.java
@@ -0,0 +1,18 @@
+
+package com.training.entity;
+
+import java.util.*;
+
+public class TourList {
+	
+	private List<Tour> TourList;
+
+	public List<Tour> getTourList() {
+		return TourList;
+	}
+
+	public void addTour(Tour tour) {
+		TourList.add(tour);
+	}
+	
+}
